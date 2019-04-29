c63966d306f0d8f108787bf00221456000533840
yaw joseph etse <yaw.etse@gmail.com>
2019-04-29 15:06:54 -0400

    added correct google cloud file path

--- utilities/pkgcloud-utils.js
+++ utilities/pkgcloud-utils.js
@@ -11,10 +11,12 @@ async function getPkgClient() {
     const containerName = extSettings.container.name;
     client = pkgcloud.storage.createClient(extSettings.client);
     if (client.provider === 'google') {
-      publicPath = { // https://storage.cloud.google.com/app.jewelml.io/clouduploads/2019/03/26/5c8fc06500edfcf79e630ce3-2019-03-26_08-48-08-jewel_logo_sm.png?_ga=2.11903795.-642431485.1553568040
-        cdnUri:  `http://storage.cloud.google.com/${containerName}`,
-        cdnSslUri:  `https://storage.cloud.google.com/${containerName}`,
-        endpoint: 'https://storage.cloud.google.com',
+      publicPath = {
+        // https://storage.cloud.google.com/app.jewelml.io/clouduploads/2019/03/26/5c8fc06500edfcf79e630ce3-2019-03-26_08-48-08-jewel_logo_sm.png?_ga=2.11903795.-642431485.1553568040
+        // https://storage.googleapis.com/app.jewelml.io/clouduploads/integrations/5cbf1464f71cae765df8bd82/5c8fc06500edfcf79e630ce3-2019-04-29_14-29-10-sample-upload.csv
+        cdnUri:  `http://storage.googleapis.com/${containerName}`,
+        cdnSslUri:  `https://storage.googleapis.com/${containerName}`,
+        endpoint: 'https://storage.googleapis.com',
       };
     } else {
       publicPath = {
